474c4bf380e39ad85400d0f2ad939d05836867e6
BenoitAune <benoit.aune@gmail.com>
2019-08-29 00:20:11 +0200

    Add files via upload

--- FEPLL/get_prior_model.m
+++ FEPLL/get_prior_model.m
@@ -21,8 +21,8 @@ function prior_model = get_prior_model(p)
 if ~exist('p', 'var')
     p = 0.95;
 end
-load('data/gmm.mat', 'GS');
-load('data/gmm_tree.mat', 'GStree');
+load('FEPLL data/gmm.mat', 'GS');
+load('FEPLL data/gmm_tree.mat', 'GStree');
 [GStree, GS]       = gstree_sv_threshold(GStree, GS, p);
 prior_model.GS     = GS;
 prior_model.GStree = GStree;
